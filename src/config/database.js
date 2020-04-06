7e79694f9dd967a06f3abda967bb9b78d668dd4c
murilio <muriliosilva201@gmail.com>
2020-04-06 18:24:54 -0300

    subindo a primeira versão da API

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: 'postgres',
-    host: 'ec2-3-229-210-93.compute-1.amazonaws.com',
-    username: 'rfrzomjniyhyld',
-    password: '4ba5459be8e76a5b654cff1d377050996d48550f7184f2869883c7d3ed094f60',
-    database: 'd4c1lh9tomok2b',
+    host: 'ec2-34-197-212-240.compute-1.amazonaws.com',
+    database: 'dd3geqpr5530qm',
+    username: 'fqgvlixprptszg',
+    password: 'fe631f06ed6c80a7f890dfeb5cc77424e10af558df5aacb2db34781795a13902',
     define: {
         timestamps: true,
         underscored: true
