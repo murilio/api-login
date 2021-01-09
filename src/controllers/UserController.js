0ecf02915bb1af8068822b6a671fdeec33b2c11d
Murilio <muriliosilva201@gmail.com>
2021-01-09 11:32:31 -0300

    criando o metodo de excluir o usuario

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -30,6 +30,20 @@ module.exports = {
     await user.update({ name, email, password }, { where: { id } })
 
     return res.status(200).json(user)
+  },
+
+  async delete (req, res) {
+    const { id } = req.params
+
+    const user = await User.findByPk(id)
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' })
+    }
+
+    await user.destroy()
+
+    return res.status(200).json({ message: 'User destroy' })
   }
 
 }
